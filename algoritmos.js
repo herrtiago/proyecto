81a1bc987cb2df1657ecce3cf4434d4f45c2b30a
Santi <herrtiago22@gmail.com>
2022-11-29 13:38:08 -0500

    primer commit

---
+++ algoritmos.js
@@ -0,0 +1,16 @@
+console.log("PUNTO 1")
+
+
+console.log("PUNTO 2")
+
+console.log("PUNTO 3")
+
+console.log("PUNTO 4")
+
+console.log("PUNTO 5")
+
+console.log("PUNTO 6")
+
+console.log("PUNTO 7")
+
+console.log("PUNTO 8")
